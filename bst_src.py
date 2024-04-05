faab8c9657af2fc1591b3a22dd25ba11d0564264
RoseLuv <marekbajak2003@gmail.com>
2024-04-05 15:27:35 +0200

    boxes, buttons, add coord to node

--- bst_src.py
+++ bst_src.py
@@ -2,29 +2,34 @@ import tkinter as tk
 import turtle as trl
 
 class Node():
-    def __init__(self, key) -> None:
+    def __init__(self, key: int | None) -> None:
         self.left: Node | None = None
         self.right: Node | None = None
-        self.key: int = key
+        self.key: int | None = key
+        self.coord: tuple[int,int] | None = None
 
 class Tree():
-    def __init__(self, node: Node) -> None:
+    def __init__(self, node: Node | None) -> None:
         self.tree_root = node
 
 def search(cur_node: Node | None, value) -> bool:
     if cur_node == None:
-        return False 
+        return False
+
+    if cur_node.key == value:
+        return True
+
     return search(cur_node.left) if value < cur_node.key else\
            search(cur_node.right)
     
-def input(cur_node: Node, value: int, tree: Tree | None) -> Tree | None:
-    if tree == None:
-        tree = Tree(cur_node)
-        return tree
+def input(cur_node: Node, value: int, tree: Tree) -> None:
+    if cur_node.key == None:
+        tree.tree_root = Node(value)
+        return
 
     if cur_node.key == value:
         print("Invalid input, key already in tree.")
-        return
+        return 
 
     if cur_node.key < value:
         if cur_node.left == None:
@@ -62,8 +67,8 @@ def find_pred(cur_node: Node) -> Node | None:
     return find_max(cur_node.left)
 
 #TODO FINISH DELETE
-def delete(cur_node: Node, tree: Tree | None, value: int) -> None:
-    if tree == None:
+def delete(cur_node: Node, value: int, tree: Tree | None,) -> None:
+    if tree.tree_root == None:
         print("Invalid, nothing to delete")
         return
     if cur_node.left.key == value:
@@ -76,11 +81,37 @@ def delete(cur_node: Node, tree: Tree | None, value: int) -> None:
         tree.tree_root = find_succ(cur_node)
 
 
-
+def list_to_int(input_str:str) -> int | None:
+    if input_str == '':
+        return 
+    # Mypy: cannot access local variable 'i' where it is not associated with a value
+    i = 0
+    result = 0
+    input_str = input_str[::-1]
+    for i in range(len(input_str) - 1):
+        result += int(input_str[i]) * (10 ** i)
+    print(i)
+    return (result * (-1)) if input_str[i] == '-' else (result + int(input_str[i]) * (10 ** i))
 
 
 def main_func():
+    tree = Tree(Node(None))
     root = tk.Tk()
+    root.geometry("%dx%d" % (root.winfo_screenwidth(), root.winfo_screenheight()))
+
+    top_frame = tk.Frame(root, width=400, height=200, background="white", borderwidth=2, relief="solid")
+    top_frame.place(x=0,y=0)
+    tk.Label(top_frame, text="Input key:", font=("Arial",20), relief="solid", borderwidth=2, height=1, width=10).grid(padx=15, pady=15, row=0, column=0)
+
+    input_box = tk.Text(top_frame, font=("Arial", 20), borderwidth=2, relief="solid", height=1, width=15)
+    input_box.grid(padx=15, pady=15, column=1, row=0, columnspan=5)
+
+    tk.Button(top_frame, text="Input", font=("Arial", 20), relief="solid",borderwidth=2,height= 1, width=10, command=lambda :input(tree.tree_root,(list_to_int(input_box.get("1.0","end-1c"))),tree)).grid( row=1, column=0, sticky="w", padx=15, pady=15)
+    tk.Button(top_frame, text="Search", font=("Arial", 20), relief="solid",borderwidth=2,height= 1, width=10, command=lambda :search(tree.tree_root,(list_to_int(input_box.get("1.0","end-1c"))))).grid(row=1, column=1, sticky="w", padx=15, pady=15)
+    tk.Button(top_frame, text="Delete", font=("Arial", 20), relief="solid",borderwidth=2,height= 1, width=10, command=lambda :delete(tree.tree_root,(list_to_int(input_box.get("1.0","end-1c"))), tree)).grid(row=2, column=0, sticky="w", padx=15, pady=15)
+
+    main_canvas = tk.Canvas(root, height=800, width=1000, relief="solid", background="white", borderwidth=2)
+    main_canvas.place(x=500,y=0)
     
     root.mainloop()
 
